d0721a03a77b014ea210a30ce8fc5f84db58e8cd
Josh Grant <Josh_grant1@baylor.edu>
2021-09-12 18:06:19 -0500

    commit

--- Dictionary_Exercise1.py
+++ Dictionary_Exercise1.py
@@ -4,10 +4,17 @@ Dict_2 = {'CS101':'Haynes','CS102':'Alvarado','CS103':'Rich','NT110':'Burke','CM
 
 Dict_3 = {'CS101':'8:00am','CS102':'9:00am','CS103':'10:00am','NT110':'11:00am','CM241':'1:00pm'}
 
+room = "Your room number is: "
+Teacher = "Your teacher is: "
+Time = "Your class time is: "
+
 def main():
     course_num = input("Enter your course number")
+    print(room)
     print(Dict_1[course_num])
+    print(Teacher)
     print(Dict_2[course_num])
+    print(Time)
     print(Dict_3[course_num])
 
 main()
