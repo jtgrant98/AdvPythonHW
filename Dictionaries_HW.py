97def46b48b5ff9fee4a2d8792ff8ce7f9d99c39
Josh Grant <Josh_grant1@baylor.edu>
2021-09-12 14:04:40 -0500

    init

--- Dictionaries_HW.py
+++ Dictionaries_HW.py
@@ -1,1 +1,13 @@
-Hello world my name is josh
+Dict_1 = {'CS101':'3004','CS102':'4501','CS103':'6755','NT110':'1244','CM241':'1411'}
+
+Dict_2 = {'CS101':'Haynes','CS102':'Alvarado','CS103':'Rich','NT110':'Burke','CM241':'Lee'}
+
+Dict_3 = {'CS101':'8:00am','CS102':'9:00am','CS103':'10:00am','NT110':'11:00am','CM241':'1:00pm'}
+
+def main():
+    course_num = input("Enter your course number")
+    print(Dict_1[course_num])
+    print(Dict_2[course_num])
+    print(Dict_3[course_num])
+
+main()
